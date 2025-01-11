2f5d6fe0b97a9a2620b8ef961c98f64c1190c8f8
Pinal120 <ptandel435@gmail.com>
2025-01-11 17:36:49 +0000

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Source: https://www.youtube.com/watch?v=ldwlOzRvYOU */
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }  
